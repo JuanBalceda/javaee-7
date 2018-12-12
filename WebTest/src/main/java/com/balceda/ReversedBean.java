fb021d9ae5ee362be65092d3e4b6112962e9410b
Juan Balceda <juanbalceda@gmail.com>
2018-12-12 01:28:13 +0100

    Module III - Class 02 examples added

---
+++ WebTest/src/main/java/com/balceda/ReversedBean.java
@@ -0,0 +1,65 @@
+package com.balceda;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+/**
+ * Created by jbalceda on Dec, 2018
+ */
+@ManagedBean
+@ViewScoped
+public class ReversedBean implements Serializable {
+
+    private String word;
+    private String reversedWord;
+    private int wordAmount;
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+    public void reverse(){
+        reversedWord = "";
+        wordAmount = 0;
+
+        if (word!=null && !word.isEmpty()){
+            wordAmount = 1;
+        }
+
+        for (int i = word.length() -1; i>=0;i--){
+            char letter = word.charAt(i);
+            reversedWord += letter;
+            if (letter==' '){
+                wordAmount++;
+            }
+        }
+        System.out.println("Reverse Word: " + reversedWord);
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getReversedWord() {
+        return reversedWord;
+    }
+
+    public void setReversedWord(String reversedWord) {
+        this.reversedWord = reversedWord;
+    }
+
+    public int getWordAmount() {
+        return wordAmount;
+    }
+
+    public void setWordAmount(int wordsAmount) {
+        this.wordAmount = wordsAmount;
+    }
+}
