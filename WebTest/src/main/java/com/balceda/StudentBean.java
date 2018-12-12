8851c89f8714843c8426ecc65f8e2d133ba78f38
Juan Balceda <juanbalceda@gmail.com>
2018-12-12 01:00:59 +0100

    Module III - Class 02

---
+++ WebTest/src/main/java/com/balceda/StudentBean.java
@@ -0,0 +1,33 @@
+package com.balceda;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+/**
+ * Created by jbalceda on Dec, 2018
+ */
+@ManagedBean
+@ViewScoped
+public class StudentBean implements Serializable {
+
+    private String name;
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void register(){
+        System.out.println("Name: "+name);
+    }
+}
