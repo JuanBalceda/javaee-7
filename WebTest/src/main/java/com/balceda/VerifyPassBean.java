58f5adf7f551c6c2a1755f5898c9e69572fe2b50
Juan Balceda <juanbalceda@gmail.com>
2018-12-13 22:08:48 +0100

    Module III - Class 03: Working with inputTextArea

--- WebTest/src/main/java/com/balceda/VerifyPassBean.java
+++ WebTest/src/main/java/com/balceda/VerifyPassBean.java
@@ -6,6 +6,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean
 @ViewScoped
@@ -13,11 +15,29 @@ public class VerifyPassBean implements Serializable {
 
     private String login;
     private String password;
+    private String name;
+    private String about;
 
     @PostConstruct
     public void init() {
         login = "";
         password = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
     }
 
     public String getLogin() {
@@ -39,5 +59,25 @@ public class VerifyPassBean implements Serializable {
     public void register() {
         System.out.println("Password: " + password);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Register completed"));
+    }
+
+    public void updateProfile() {
+        System.out.println("Name: " + name);
+        System.out.println("About: " + about);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Profile completed"));
+    }
+
+    public List<String> autocompleteOptions(String text){
+        List<String> list = new ArrayList<>();
+
+        // Works with at least 3 characters
+        if (text.startsWith("joa")){
+            list.add("Joan");
+            list.add("Joana");
+            list.add("Joa");
+            list.add("Joane");
+            list.add("Joaquin");
+        }
+        return list;
     }
 }
